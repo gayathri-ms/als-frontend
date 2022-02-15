ddfe3569d2b6662ffaec9ca99e52637a28936a06
gayathri-ms <ms.gayathri266520@gmail.com>
2022-02-16 00:37:00 +0530

    updated

--- src/components/variables.js
+++ src/components/variables.js
@@ -1,3 +1,3 @@
-export const API = "https://amman-lorry-service.herokuapp.com/";
+export const API = "https://amman-lorry-service.herokuapp.com";
 // export const frontend = "http://localhost:3001/";
 export const frontend = "https://amman-lorry-service.vercel.app/";
